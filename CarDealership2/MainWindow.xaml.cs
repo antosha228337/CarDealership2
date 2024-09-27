66acc84038d9684c3a288ef60e6230039cd85a73
Антон Шаров <vedrocow644@gmail.com>
2024-09-27 09:59:37 +0300

    отображение справочника car и modification в виде таблицы и связанной с ним сущности

--- CarDealership2/MainWindow.xaml.cs
+++ CarDealership2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace CarDealership2
 {
@@ -16,9 +19,28 @@ namespace CarDealership2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string connectionString;
+
+        private NpgsqlDataAdapter carAdapter;
+        private NpgsqlDataAdapter modificationAdapter;
+
+        private DataSet dataSet = new DataSet(); 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+            carAdapter = new NpgsqlDataAdapter("SELECT * FROM car;", connectionString);
+            modificationAdapter = new NpgsqlDataAdapter("SELECT * FROM modification;", connectionString);
+
+            carAdapter.Fill(dataSet, "car");
+            modificationAdapter.Fill(dataSet, "modification");
+
+            dataGridCar.ItemsSource = dataSet.Tables["car"]?.DefaultView;
+
+            dataGridMod.ItemsSource = dataSet.Tables["modification"]?.DefaultView;
         }
     }
 }
